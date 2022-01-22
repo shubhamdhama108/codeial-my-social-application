a42b7b1720d6b47f757c71c249c03d8a3b7ed35a
shubham dhama <dhamashubham107@gmail.com>
2022-01-22 19:59:29 +0530

    manual auth

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,9 +2,19 @@ const User = require('../models/user');
 
 module.exports.profile= function(req,res){
 
-    return res.render('user_profile',{  
-        title:"Shubham"
-    });
+   if(req.cookies.user_id){
+       User.findById(req.cookies.user_id,function(err,user){
+           if(user){
+               return res.render('user_profile',{
+                   title:"User Profile",
+                   user:user
+               })
+           }
+           return res.redirect('/users/sign-in');
+       })
+   }else{
+         return res.redirect('/users/sign-in');
+   }
 
 
     // return res.end('<h1>User Profile!</h1>');
@@ -48,5 +58,28 @@ module.exports.create = function(req,res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req,res){
+
+    // find the user
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        // handle user found
+
+        if(user){
+            // handle password which doesn't match
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+
+            // handle session creation
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+
+        }else{
+            // handle user not found
+            return res.redirect('back');
+        }
+    });
     
 }
+
