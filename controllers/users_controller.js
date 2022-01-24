37c6dea92231465f999aa138667691b369caf77e
shubham dhama <dhamashubham107@gmail.com>
2022-01-24 18:32:09 +0530

    add sign out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -12,6 +12,9 @@ module.exports.profile= function(req,res){
 
 // render the sign up page
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title:"Codeial | SignUp"
     });
@@ -19,6 +22,9 @@ module.exports.signUp = function(req,res){
 
 // render the sign in page
 module.exports.signIn = function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title:"Codeial | SignIn"
     });
@@ -48,5 +54,11 @@ module.exports.create = function(req,res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req,res){
-    
+    return res.redirect('/');
+}
+
+module.exports.destroySession = function(req,res){
+    req.logout();
+
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -4,6 +4,34 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+const session = require('express-session');
+const req = require('express/lib/request');
+const passport = require('passport');
+const passportLocal = require('./config/passport-local-strategy');
+const MongoStore = require('connect-mongo');
+
+
+app.use(session({
+    name:'codeial',
+    secret:'blahsomething',
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        maxAge:(1000*60*100)
+    },
+    store: MongoStore.create(
+        {
+            mongoUrl:'mongodb://localhost/codeial_development'
+        },
+        function(err){
+            console.log(err||'connect-mongodb setup ok');
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(passport.setAuthenticatedUser);
 
 
 app.use(express.urlencoded());
